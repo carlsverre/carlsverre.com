9e671ca4f0cacd7a27c67fba870766b4317ff5f7
Carl Sverre <accounts@carlsverre.com>
2017-09-24 16:01:20 -0700

    switching over to proto

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,23 @@
 
 set -e
 
+REMOTE=proto
+ROOT=/srv/www-carlsverre-com
+
 echo "Updating wall drawing 91"
 ( cd art/wall_drawing_91 && npm run build )
-ssh ralph "mkdir -p static/www-carlsverre-com/art/wall_drawing_91"
-scp -r art/wall_drawing_91/{index.html,assets} ralph:static/www-carlsverre-com/art/wall_drawing_91
+ssh ${REMOTE} "mkdir -p ${ROOT}/art/wall_drawing_91"
+scp -r art/wall_drawing_91/{index.html,assets} ${REMOTE}:${ROOT}/art/wall_drawing_91
 
 echo "Updating colorful hexagons"
 ( cd art/hexagon_colors && npm run build )
-ssh ralph "mkdir -p static/www-carlsverre-com/art/hexagon_colors"
-scp -r art/hexagon_colors/{index.html,assets} ralph:static/www-carlsverre-com/art/hexagon_colors
+ssh ${REMOTE} "mkdir -p ${ROOT}/art/hexagon_colors"
+scp -r art/hexagon_colors/{index.html,assets} ${REMOTE}:${ROOT}/art/hexagon_colors
 
 echo "Updating worldspin"
 ( cd art/worldspin && npm run build )
-ssh ralph "mkdir -p static/www-carlsverre-com/art/worldspin"
-scp -r art/worldspin/build/* ralph:static/www-carlsverre-com/art/worldspin
+ssh ${REMOTE} "mkdir -p ${ROOT}/art/worldspin"
+scp -r art/worldspin/build/* ${REMOTE}:${ROOT}/art/worldspin
 
 echo "Updating main assets"
-scp -r index.html assets ralph:static/www-carlsverre-com
+scp -r index.html assets ${REMOTE}:${ROOT}
