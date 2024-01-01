07516e3619fae8711acfeab23bf843aeb8e1023c
Carl Sverre <82591+carlsverre@users.noreply.github.com>
2023-12-31 16:32:24 -0800

    should work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://carlsverre.com",
   integrations: [mdx(), sitemap()],
   prefetch: true,
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
